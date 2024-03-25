dfe10e583514328dd49c6ec5b5ae44594aeb34cf
Yishak T. Kidanu <80487041+Yishak-KD@users.noreply.github.com>
2024-03-26 00:05:03 +0300

    Delete util directory

--- util/ResponseValidation.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosResponse } from 'axios'
-
-export const isSuccessfullStatus = (response: AxiosResponse): boolean => {
-    return (
-        Boolean(response.status) &&
-        response.status >= 200 &&
-        response.status < 300
-    )
-}
