c5bc1ad7168af178eb564c5699c06f70bd9f7954
Yishak Kidanu <yisak04@gmail.com>
2024-03-28 12:58:05 +0300

    Font size for card update

--- src/components/CardSection.tsx
+++ src/components/CardSection.tsx
@@ -47,7 +47,7 @@ const CardSection = ({ items }: CardItemProps) => {
                     <div className="lg:relative flex flex-col xl:w-[18%] w-full" key={id}>
                         <div className={`lg:absolute ${item.id === 3 ? 'lg:-top-64 rounded-3xl' : 'lg:-top-80 rounded-3xl'}`} style={{ backgroundImage: `url(${item.backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
                             <div className={`relative flex flex-col items-center justify-center ${item.id === 3 ? 'h-64' : 'h-80'}`}>
-                                <h1 className={`font-bold text-center ${item.id === 2 || item.id === 4 ? 'lg:mt-[-180px] ' : 'mt-0'} mx-auto xl:text-4xl lg:text-2xl xl:lg:px-0 px-4`}>{item.title}</h1>
+                                <h1 className={`font-bold text-center ${item.id === 2 || item.id === 4 ? 'lg:mt-[-180px] ' : 'mt-0'} mx-auto xl:text-4xl lg:text-2xl text-4xl xl:lg:px-0 px-4`}>{item.title}</h1>
                                 {item.image && <img src={item.image} alt="" height={110} width={110} className="lg:absolute lg:mt-10" />}
                                 <div className={`cursor-pointer absolute flex lg:flex-row items-center border-[#fff] py-2 px-2 rounded-full ${item.id === 4 ? 'bg-transparent' : 'bg-[#D7E5EC]'} w-[90%] justify-between bottom-2`}
                                     onClick={() => window.location.replace(item.redirect)}>
