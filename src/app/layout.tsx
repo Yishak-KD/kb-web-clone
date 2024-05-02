414d3e1c62356c986d2e32312c6680eeba5f0bca
Dnathan33 <nathnaelmelkamu@gmail.com>
2024-05-01 22:13:05 -0700

    added faviicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,82 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Head from "next/head";
-import { Metadata } from "next";
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Head from 'next/head';
+import { Metadata } from 'next';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Kedus Bible',
-  description: 'The Kedus Bible is a mobile app that offers the Bible in various languages.',
+  description:
+    'Read the Bible in Amharic, Afaan Oromo, Tigrigna. Daily Devotionals.',
   metadataBase: new URL('https://kedusbible.com'),
-  icons: {
-    icon: '/images/favicon-32x32.ico',
-    shortcut: '/images/favicon-32x32.ico',
-    apple: '/images/apple-touch-icon.ico',
-  },
+  icons: [
+    {
+      rel: 'apple-touch-icon',
+      sizes: '57x57',
+      url: '/images/favicon/apple-icon-57x57.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '60x60',
+      url: '/images/favicon/apple-icon-60x60.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '72x72',
+      url: '/images/favicon/apple-icon-72x72.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '76x76',
+      url: '/images/favicon/apple-icon-76x76.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '114x114',
+      url: '/images/favicon/apple-icon-114x114.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '120x120',
+      url: '/images/favicon/apple-icon-120x120.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '144x144',
+      url: '/images/favicon/apple-icon-144x144.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '152x152',
+      url: '/images/favicon/apple-icon-152x152.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/images/favicon/apple-icon-180x180.png',
+    },
+    {
+      rel: 'icon',
+      sizes: '192x192',
+      url: '/images/favicon/android-icon-192x192.png',
+    },
+    {
+      rel: 'icon',
+      sizes: '32x32',
+      url: '/images/favicon/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      sizes: '96x96',
+      url: '/images/favicon/favicon-96x96.png',
+    },
+    {
+      rel: 'icon',
+      sizes: '16x16',
+      url: '/images/favicon/favicon-16x16.png',
+    },
+  ],
   appleWebApp: {
     capable: true,
     title: 'Kedus Bible',
@@ -24,7 +87,8 @@ export const metadata: Metadata = {
   },
   twitter: {
     site: 'Kedus Bible',
-    description: 'The Kedus Bible is a mobile app that offers the Bible in various languages.',
+    description:
+      'The Kedus Bible is a mobile app that offers the Bible in various languages.',
     title: 'Kedus Bible',
     images: '/images/apple-icon.ico',
   },
@@ -32,7 +96,8 @@ export const metadata: Metadata = {
     type: 'website',
     url: 'https://kedusbible.com',
     title: 'Kedus Bible',
-    description: 'The Kedus Bible is a mobile app that offers the Bible in various languages.',
+    description:
+      'The Kedus Bible is a mobile app that offers the Bible in various languages.',
     siteName: 'Kedus Bible',
     images: [
       {
@@ -51,44 +116,43 @@ export const metadata: Metadata = {
     'Old Testament',
     'New Testament',
     'Bible',
+    'amharic bible',
+    'oromo bible',
+    'afaan oromoo bible',
+    'tigrigna bible',
   ],
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
-    <html lang="en">
+    <html lang='en'>
       <Head>
         <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1 viewport-fit=cover"
+          name='viewport'
+          content='width=device-width, initial-scale=1, maximum-scale=1 viewport-fit=cover'
         />
-        <link rel="shortcut icon" href="/images/favicon-16x16.ico" />
+        <link rel='shortcut icon' href='/images/favicon-16x16.ico' />
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/apple-touch-icon.ico"
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/images/apple-touch-icon.ico'
         />
+        <link rel='apple-icon' sizes='180x180' href='/images/apple-icon.ico' />
         <link
-          rel="apple-icon"
-          sizes="180x180"
-          href="/images/apple-icon.ico"
-        />
-        <link
-          rel="icon"
-          type="image/ico"
-          sizes="32x32"
-          href="/images/favicon-32x32.ico"
+          rel='icon'
+          type='image/ico'
+          sizes='32x32'
+          href='/images/favicon-32x32.ico'
         />
         <link
-          rel="icon"
-          type="image/ico"
-          sizes="16x16"
-          href="/images/favicon-16x16.ico"
+          rel='icon'
+          type='image/ico'
+          sizes='16x16'
+          href='/images/favicon-16x16.ico'
         />
       </Head>
       <body className={inter.className}>{children}</body>
